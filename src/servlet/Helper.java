69c0bd088d5547f04924abf03483277106739d64
Tamino Hartmann <tamino.hartmann@gmail.com>
2012-02-06 18:15:02 +0100

    Servlet Helper hinzugefügt

---
+++ src/servlet/Helper.java
@@ -0,0 +1,30 @@
+/**
+ * @author Tamino Hartmann
+ */
+package servlet;
+
+/**
+ *
+ */
+public final class Helper {
+	/**
+	 * Leerer, privater Konstruktor. Es sollte von dieser Klasse keine Instanzen
+	 * geben.
+	 */
+	private Helper() {
+
+	}
+
+	// Notiz fuer alle Pfadnamen: ist ein Link ein direkter Link (zeigt also
+	// direkt auf eine Datei und nich auf einen Servlet), so beginnt dieser mit
+	// "D_".
+
+	/**
+	 * Systemunabhaengiger Pfad zum Projektordner.
+	 */
+	public static final String PROJECT_PATH = "/hiwi/";
+	/**
+	 * Pfad zum Index direkt.
+	 */
+	public static final String D_INDEX = "/hiwi/index.jsp";
+}
