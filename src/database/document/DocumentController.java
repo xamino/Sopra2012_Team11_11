79d70b3db456f0bc4132f6330e61a083e29ddcc5
Patryk Boczon <cajax@gmx.de>
2012-02-07 18:29:37 +0100

    database controller Klassen - Methoden hinzugefuegt (leer)

--- src/database/document/DocumentController.java
+++ src/database/document/DocumentController.java
@@ -1,9 +1,46 @@
+/**
+ * @author Patryk Boczon
+ */
+
 package database.document;
 /**
  * Verwaltet alle Datenbankzugriffe auf Unterlagen-bezogene Daten
  */
+import java.util.Vector;
+
 import database.DatabaseController;
 
 public class DocumentController {
 	public DatabaseController dbc;
+	
+	
+	public void createDocument(Document document){
+		
+	}
+	
+	public void deleteDocument(Document document){
+		
+	}
+	
+	public void updateDocument(Document document){
+		
+	}
+	
+	public Vector<Document> getDocumentsByOffer(int aid){
+		return null;
+		
+	}
+	
+	public Vector<Document> getAllDocuments(){
+		return null;
+		
+	}
+	
+	public Document getInstance(){
+		return null;
+		
+	}
+	
+	
+	
 }
