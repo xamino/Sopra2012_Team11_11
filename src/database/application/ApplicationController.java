941103dfcc7fc0ca1a3760260867d591cc09c75a
Oemer Sahin <bozo40@live.de>
2012-02-10 13:11:27 +0100

    Account-, Applicatio-, Documentation-, Offer-Controller wurden geandert; Konstruktoren, getInstance Methoden; Javadoc.

--- src/database/application/ApplicationController.java
+++ src/database/application/ApplicationController.java
@@ -1,49 +1,131 @@
 /**
  * @author Patryk Boczon
+ * @author Oemer Sahin
  */
 
 package database.application;
+
 /**
  * Verwaltet alle Datenbankzugriffe auf Bewerbungs-bezogene Daten
  */
 import java.util.Vector;
 
 import database.DatabaseController;
+import database.offer.OfferController;
 
 public class ApplicationController {
+
+	/**
+	 * Privater Konstruktor, da die Klasse selbst ein Singleton ist.
+	 */
+	private ApplicationController() {
+
+	}
+
+	/**
+	 * Klassenattribut "appcontr" beinhaltet eine ApplicationController-Instanz,
+	 * falls keine vorhanden war und mit der Methode getInstance angelegt wird.
+	 * Dies dient um zu gewaehrleisten, dass nur eine Instanz von
+	 * ApplicationController existiert.
+	 */
+	private static ApplicationController appcontr;
+
+	/**
+	 * Diese Instanz dient zum Zugang in die Datenbank.
+	 */
 	public DatabaseController dbc;
-	
-	public void createApplication(Application application){
-		
+
+	/**
+	 * Diese Methode erstellt ein Bewerbungsobjekt in der Datenbank. Mit
+	 * ubergebenem Bewerbungsobjekt.
+	 * 
+	 * @param application
+	 *            Parameter "application" ist ein Application-Objekt mit allen
+	 *            dazugehoerigen Attributen.
+	 */
+	public void createApplication(Application application) {
+
 	}
-	
-	public void deleteApplication(Application application){
-		
+
+	/**
+	 * Diese Methode loescht ein Bewerbungsobjekt aus der Datenbank. Mit
+	 * ubergebenem Bewerbungsobjekt.
+	 * 
+	 * @param application
+	 *            Parameter "application" ist ein Application-Objekt mit allen
+	 *            dazugehoerigen Attributen.
+	 */
+	public void deleteApplication(Application application) {
+
 	}
-	
-	public void updateApplication(Application application){
-		
+
+	/**
+	 * Diese Methode aendert die Attribute einer Bewerbung bzw. aktualisiert
+	 * diese in der Datenbank.
+	 * 
+	 * @param application
+	 *            Parameter "application" ist ein Application-Objekt mit allen
+	 *            dazugehoerigen Attributen.
+	 */
+	public void updateApplication(Application application) {
+
 	}
-	
-	public Vector<Application> getAllApplications(){
+
+	/**
+	 * Diese Methode sammelt alle Bewerbungen aus der Datenbank und speichert
+	 * diese in einem Vektor.
+	 * 
+	 * @return Es wird ein Vektor mit allen vorhanden Bewerbungen in der
+	 *         Datenbank zurueckgegeben.
+	 */
+	public Vector<Application> getAllApplications() {
 		return null;
-		
+
 	}
-	
-	public Vector<Application> getApplicationsByApplicant(String username){
+
+	/**
+	 * Diese Methode sammelt alle Bewerbungen eines bestimmten Bewerbers aus der
+	 * Datenbank und speichert diese in einem Vektor.
+	 * 
+	 * @param username
+	 *            Parameter "username" gibt den Namen dese Bewerbers an, dessen
+	 *            Bewerbungen angezeigt werden sollen.
+	 * @return Alle Bewerbungen zu einem Bewerber aus der Datenbank in Form
+	 *         eines Vektors werden zurueckgegeben.
+	 */
+	public Vector<Application> getApplicationsByApplicant(String username) {
 		return null;
-		
+
 	}
-	
-	public Vector<Application> getApplicationsByOffer(int aid){
+
+	/**
+	 * Diese Methode sammelt alle Bewerbungen zu einem bestimmten Jobangebot aus
+	 * der Datenbank und speichert diese in einem Vektor.
+	 * 
+	 * @param aid
+	 *            Parameter "aid" (Angebots-Id) ist die Id des Jobangebots.
+	 * @return Es wird ein Vektor mit allen Bewerbungen zu einem bestimmten
+	 *         Jobangebot aus der Datenbank zurueckgegeben.
+	 */
+	public Vector<Application> getApplicationsByOffer(int aid) {
 		return null;
-		
+
 	}
-	
-	public Application getInstance(){
-		return null;
-		
+
+	/**
+	 * Diese Methode prueft ob ein ApplicationController-Objekt existiert. Falls
+	 * nicht wird eine neue ApplicationOffer-Instanz angelegt, zurueckgegeben
+	 * und in dem Klassenattribut "appcontr" abgespeichert. Dies dient um zu
+	 * gewaehrleisten, dass nur eine Instanz von ApplicationController
+	 * existiert.
+	 * 
+	 * @return Es wird die Instanz zurueckgegeben.
+	 */
+	public static ApplicationController getInstance() {
+		if (appcontr == null)
+			appcontr = new ApplicationController();
+		return appcontr;
+
 	}
-	
-	
+
 }
--- src/database/document/DocumentController.java
+++ src/database/document/DocumentController.java
@@ -1,5 +1,6 @@
 /**
  * @author Patryk Boczon
+ * @author Oemer Sahin
  */
 
 package database.document;
@@ -10,44 +11,114 @@ import java.util.Vector;
 
 import database.DatabaseController;
 import database.account.Account;
+import database.application.ApplicationController;
 import database.offer.Offer;
 
 public class DocumentController {
+	
 	public DatabaseController dbc;
 	
+	/**
+	 * Klassenattribut "doccontr" beinhaltet eine ApplicationController-Instanz,
+	 * falls keine vorhanden war und mit der Methode getInstance angelegt wird.
+	 * Dies dient um zu gewaehrleisten, dass nur eine Instanz von
+	 * ApplicationController existiert.
+	 */
+	private static DocumentController doccontr;
 	
+	/**
+	 * Privater Konstruktor, da die Klasse selbst ein Singleton ist.
+	 */
+	private DocumentController(){
+		
+	}
+		
+	/**
+	 * Diese Methode erstellt ein Unterlagen-Objekt (Document-Objekt) in der Datenbank. Mit
+	 * ubergebenem Unterlagen-Objekt (Document-Objekt).
+	 * @param document
+	 * Parameter "document" ist ein Document-Objekt mit allen
+	 *            dazugehoerigen Attributen.
+	 */
 	public void createDocument(Document document){
 		
 	}
 	
+	/**
+	 * Diese Methode loescht ein Unterlagen-Objekt (Document-Objekt) aus der Datenbank.
+	 * @param document
+	 * Parameter "document" ist ein Document-Objekt mit allen
+	 *            dazugehoerigen Attributen.
+	 */
 	public void deleteDocument(Document document){
 		
 	}
 	
+	/**
+	 * Diese Methode aendert die Attribute einer Unterlage bzw. aktualisiert diese in der Datenbank.
+	 * @param document
+	 * Parameter "document" ist ein Document-Objekt mit allen
+	 *            dazugehoerigen Attributen.
+	 */
 	public void updateDocument(Document document){
 		
 	}
 	
+	/**
+	 * Diese Methode sammelt alle Unterlagen zu einem bestimmten Jobangebot aus
+	 * der Datenbank und speichert diese in einem Vektor.
+	 * 
+	 * @param aid
+	 *            Parameter "aid" (Angebots-Id) ist die Id des Jobangebots.
+	 * @return Es wird ein Vektor mit allen Unterlagen zu einem bestimmten
+	 *         Jobangebot aus der Datenbank zurueckgegeben.
+	 */
 	public Vector<Document> getDocumentsByOffer(int aid){
 		return null;
 		
 	}
 	
+	/**
+	 * Diese Methode sammelt alle Unterlagen von einem bestimmten Account zu einem bestimmten Jobangebot.
+	 * @param account
+	 * 	Parameter "account" ist ein Account-Objekt mit allen Account-Attributen.
+	 * @param offer
+	 * 	Parameter "offer" ist ein Offer-Objekt mit allen Offer-Attributen.
+	 * @return
+	 * Es wird ein Vektor mit allen Unterlagen von einem bestimmten Account zu einem bestimmten Jobangebot aus der Datenbank zurueckgegeben.
+	 */
 	public Vector<Document> getDocumentsByUserAndOffer(Account account, Offer offer){
 		return null;
 		
 	}
 	
+	/**
+	 * Diese Methode sammelt alle Unterlagen aus der Datenbank und speichert
+	 * diese in einem Vektor.
+	 * 
+	 * @return Es wird ein Vektor mit allen vorhanden Unterlagen aus der
+	 *         Datenbank zurueckgegeben.
+	 */
 	public Vector<Document> getAllDocuments(){
 		return null;
 		
 	}
 	
-	public Document getInstance(){
-		return null;
 		
+	/**
+	 * Diese Methode prueft ob ein DocumentController-Objekt existiert. Falls
+	 * nicht wird eine neue DocumentController-Instanz angelegt, zurueckgegeben
+	 * und in dem Klassenattribut "doccontr" abgespeichert. Dies dient um zu
+	 * gewaehrleisten, dass nur eine Instanz von DocumentController
+	 * existiert.
+	 * 
+	 * @return Es wird die Instanz zurueckgegeben.
+	 */
+	public static DocumentController getInstance() {
+		if (doccontr == null)
+			doccontr = new DocumentController();
+		return doccontr;
+
 	}
-	
-	
 	
 }
