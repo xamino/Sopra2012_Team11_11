8cd3f05fc724950eabe7e96b4a2c94bb18fc8f4c
Anatoli Brill <Anatoli.Brill@uni-ulm.de>
2012-02-13 16:46:46 +0100

    AppDoc konstruktor get und set

--- src/database/document/AppDocument.java
+++ src/database/document/AppDocument.java
@@ -1,5 +1,6 @@
 /**
  * @author Manuel Guentzel
+ * @author Anatoli Brill
  */
 
 package database.document;
@@ -9,5 +10,121 @@ package database.document;
  * Dokumenten, welche der Bewerber dem Sachbearbeiter zukommen lassen muss.
  */
 public class AppDocument {
+
+	/**
+	 * Der Benutzername vom Benutzer.
+	 */
+	private String username;
+	/**
+	 * Die ID des Angebots.
+	 */
+	private int offerID;
+	/**
+	 * Die ID der Unterlage.
+	 */
+	private int docID;
+	/**
+	 * Falls die Unterlage abgegeben wurde, wird es auf <code>TRUE</code>
+	 * gesetzt. Ansonsten auf <code>FALSE</code>.
+	 */
+	private boolean present;
+
+	/**
+	 * Konstruktor fuer die Bewerbungsunterlagen.
+	 * 
+	 * @param username
+	 *            Benutzername.
+	 * @param aID
+	 *            Angebots ID.
+	 * @param uID
+	 *            Unterlagen ID.
+	 * @param present
+	 *            Falls Unterlage vorhanden <code>TRUE</code> ansonsten
+	 *            <code>FALSE</code>.
+	 */
+	public AppDocument(String username, int aID, int uID, boolean present) {
+		super();
+		this.username = username;
+		this.offerID = aID;
+		this.docID = uID;
+		this.present = present;
+	}
+
+	/**
+	 * Hollt den Benutzernamen.
+	 * 
+	 * @return Benutzername.
+	 */
+	public String getUsername() {
+		return username;
+	}
+
+	/**
+	 * Setzt den Benutzername.
+	 * 
+	 * @param Benutzername
+	 *            .
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	/**
+	 * Hollt die AngebotsID.
+	 * 
+	 * @return AngebotsID.
+	 */
+	public int getoID() {
+		return offerID;
+	}
+
+	/**
+	 * Setzt die AngebotsID.
+	 * 
+	 * @param AngebotsID
+	 *            .
+	 */
+	public void setoID(int oID) {
+		this.offerID = oID;
+	}
+
+	/**
+	 * Hollt die UnterlagenID.
+	 * 
+	 * @return UnterlagenID.
+	 */
+	public int getdID() {
+		return docID;
+	}
+
+	/**
+	 * Setzt die UnterlagenID.
+	 * 
+	 * @param UnterlagenID
+	 *            .
+	 */
+	public void setdID(int dID) {
+		this.docID = dID;
+	}
+
+	/**
+	 * Hollt die Information ob die Unterlage vorhanden ist.
+	 * 
+	 * @return <code>TRUE</code> Unterlage vorhanden, sonst <code>FALSE</code>.
+	 */
+	public boolean getPresent() {
+		return present;
+	}
+
+	/**
+	 * Setzt die Information ob die Unterlage vorhanden ist.
+	 * 
+	 * @param present
+	 *            <code>TRUE</code> Unterlage vorhanden, sonst
+	 *            <code>FALSE</code>.
+	 */
+	public void setPresent(boolean present) {
+		this.present = present;
+	}
 
 }
