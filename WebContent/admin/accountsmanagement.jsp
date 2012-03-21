b18e9c2ca8f4ac54ff338a09ad25f29f292d7420
Tamino Hartmann <tamino.hartmann@gmail.com>
2012-03-21 14:49:30 +0100

    Views: Added basic script.js to public, admin, and clerk. No feature implementation yet.

--- WebContent/admin/accountsmanagement.jsp
+++ WebContent/admin/accountsmanagement.jsp
@@ -1,18 +1,14 @@
 <%@page import="servlet.Helper"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
-
 <!DOCTYPE HTML PUBLIC "-//W3C/DTD HTML 4.01 Transitional/EN" "http://www.w3.org/TR/html4/loose.dtd">
-
 <html>
-
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <link rel="stylesheet" type="text/css" href="../style.css" />
 <script type="text/javascript" src="script.js"></script>
 <title>Account Verwaltung | Hiwi Job Börse</title>
 </head>
-
 <body>
 	<div class="header">
 		<h1>
---
+++ WebContent/admin/script.js
@@ -0,0 +1,45 @@
+/**
+ * Script for admin webpages.
+ * @author: Tamino Hartmann
+ */
+
+/**
+ * Get XMLHttpRequest object for all important browsers.
+ * 
+ * @returns XMLHttpRequest object.
+ */
+function getXMLObject() {
+	// Code for IE7+, Firefox, Chrome, Opera, Safari
+	if (window.XMLHttpRequest) {
+		return new XMLHttpRequest();
+	}
+	// Code for IE6, IE5
+	else {
+		return new ActiveXObject("Microsoft.XMLHTTP");
+	}
+}
+
+// Required for all calls:
+var xmlhttp = new getXMLObject();
+
+/**
+ * Use this function to toggle the visibility of error messages in error divs.
+ * 
+ * @param id
+ *            The ID of the div which to toggle.
+ * @param flag
+ *            If <code>true</code> show, with <code>false</code> hide.
+ * @param text
+ *            Text to input. If flag is false, won't be set.
+ */
+function toggleWarning(id, flag, text) {
+	// alert("ID: " + id + "\nFLAG: " + flag);
+	if (flag == true) {
+		document.getElementById(id).setAttribute("class", "visibleerror");
+		document.getElementById(id).innerHTML = text;
+	} else {
+		document.getElementById(id).setAttribute("class", "hiddenerror");
+		// By false, keep old text (won't be seen anyway):
+		// document.getElementById(id).innerHTML = text;
+	}
+}
