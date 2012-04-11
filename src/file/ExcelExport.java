746579742e36fa029a18a72b13f9e5ef5b09cdde
Manuel Güntzel <manuel.guentzel@uni-ulm.de>
2012-04-11 13:05:33 +0200

    excel export an die richtige stelle verschoben

--- src/file/ExcelExport.java
+++ src/file/ExcelExport.java
@@ -1,5 +1,17 @@
 package file;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Vector;
+
+import jxl.Workbook;
+import jxl.write.Label;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
+import jxl.write.WriteException;
+import jxl.write.biff.RowsExceededException;
+import database.application.Application;
+import database.application.ApplicationController;
 import user.UserData;
 
 /**
@@ -11,6 +23,11 @@ import user.UserData;
  */
 public abstract class ExcelExport {
 	/**
+	 * 
+	 */
+	private static ApplicationController appcon=ApplicationController.getInstance();
+	
+	/**
 	 * Erstellt eine neue Excel Datei mit den getaetigten Einstellungen des
 	 * Verwalters und gibt den Link dazu zurueck. Dem uebergebenen
 	 * UserData-Objekt werden der Benutzername und die Session entnommen um
@@ -19,9 +36,33 @@ public abstract class ExcelExport {
 	 * 
 	 * @param data
 	 *            UserData Objekt des Benutzers.
+	 * @throws IOException 
+	 * @throws WriteException 
+	 * @throws RowsExceededException 
 	 */
-	public static String export(final UserData data) {
-		return null;
+	public static String export(final UserData data) throws IOException, RowsExceededException, WriteException {
+		String clerkname = data.getUsername(); 
+		Vector<Application> appvec = appcon.getApprovedApplicationsByClerk(clerkname);
+		
+		WritableWorkbook ww = Workbook.createWorkbook(new File("ExcelExport.xls"));
+		WritableSheet sh = ww.createSheet("All Applications by "+clerkname, 0);
+		
+		Label Name = new Label(0,0,"Name");
+		Label id = new Label(0,1,"ID");
+		sh.addCell(Name);
+		sh.addCell(id);
+		
+		for (int i = 0; i < appvec.size(); i++) {
+			
+			Label aID = new Label(i,1,""+appvec.get(i).getAid());
+			Label aName = new Label(i,0,""+appvec.get(i).getUsername());
+			sh.addCell(aID);
+			sh.addCell(aName);
+		}
+		
+		ww.write();
+		ww.close();
+		return "Excel Tabelle erstellt. Download noch nicht bereit";
 
 	}
 
--- src/user/Clerk.java
+++ src/user/Clerk.java
@@ -16,6 +16,7 @@ import database.DatabaseController;
 import database.account.Account;
 import database.application.Application;
 import database.application.ApplicationController;
+import file.ExcelExport;
 
 /**
  * Verwaltet alle Aufgaben und Daten eines Verwalters.
@@ -125,28 +126,8 @@ public class Clerk extends User {
 	 * @throws RowsExceededException 
 	 */
 
-	public String doExport(String clerkname) throws IOException, RowsExceededException, WriteException {
-		Vector<Application> appvec = appcon.getApprovedApplicationsByClerk(clerkname);
-		
-		WritableWorkbook ww = Workbook.createWorkbook(new File("ExcelExport.xls"));
-		WritableSheet sh = ww.createSheet("All Applications by "+clerkname, 0);
-		
-		Label Name = new Label(0,0,"Name");
-		Label id = new Label(0,1,"ID");
-		sh.addCell(Name);
-		sh.addCell(id);
-		
-		for (int i = 0; i < appvec.size(); i++) {
-			
-			Label aID = new Label(i,1,""+appvec.get(i).getAid());
-			Label aName = new Label(i,0,""+appvec.get(i).getUsername());
-			sh.addCell(aID);
-			sh.addCell(aName);
-		}
-		
-		ww.write();
-		ww.close();
-		return "Excel Tabelle erstellt. Download noch nicht bereit";
+	public String doExport() throws IOException, RowsExceededException, WriteException {
+		return ExcelExport.export(this.getUserData());
 
 	}
 
