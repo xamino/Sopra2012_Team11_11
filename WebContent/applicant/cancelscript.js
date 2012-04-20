37346e3114b2a637791419d7fae3c72240c1211d
Oemer Sahin <bozo40@live.de>
2012-04-20 13:12:00 +0200

    cancelscript angepasst und kommentare geaddet

--- WebContent/applicant/cancelscript.js
+++ WebContent/applicant/cancelscript.js
@@ -5,24 +5,23 @@
  * @author: Oemer Sahin
  */
 
-
 /**
- * This function simply deletes an application. If it receives an error back from
- * the server, it will display it in an alert. WARNING: should not be called
- * directly!
+ * This function simply deletes an application. If it receives an error back
+ * from the server, it will display it in an alert. WARNING: should not be
+ * called directly!
  * 
  * @param benutzername
  *            The username of the applicant.
  * @param aid
- * 			The ID of the application which should be deleted.
+ *            The ID of the application which should be deleted.
  * @param callback
  *            The function to call when done.
  */
 
-
 function deleteApplication(aid) {
-	//callback = t_callback;
-	connect("/hiwi/Applicant/js/deleteApplication","aid="+aid, handleDeleteApplicationResponse);
+	// callback = t_callback;
+	connect("/hiwi/Applicant/js/deleteApplication", "aid=" + aid,
+			handleDeleteApplicationResponse);
 }
 
 /**
@@ -33,10 +32,14 @@ function deleteApplication(aid) {
 function handleDeleteApplicationResponse(mime, data) {
 	if (mime == null)
 		callback();
-	else if (mime == "text/url") {
-		window.location = data;
-	} else if (mime == "text/error") {
-		alert(data);
+	else if (mime == "text/url") {	// aktuell vom ApplicationServlet zurueckgeschickte response
+		window.location = data; 	// fuer Weiterleitungen auf andere,interne Seiten(z.B. index.jsp)
+		
+	} else if (mime == "text/text") {// aktuell vom ApplicationServlet zurueckgeschickte response 
+		alert("Bewerbung wurde erfolgreich widerrufen!"); //  fuer den erfolgreichen Loeschvorgang
+		
+	} else if (mime == "text/error") {// aktuell vom ApplicationServlet zurueckgeschickte response
+		alert(data); 				 // fuer Fehler die bei der Operation.
 	} else
 		callback();
 }
