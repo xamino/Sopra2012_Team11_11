ce9c1142b45dcf874008a05f1874383e4eb71130
Tamino Hartmann <tamino.hartmann@gmail.com>
2012-04-20 20:24:44 +0200

    Beispiel fehlerbehandlung für Manu eingebaut

--- WebContent/applicant/accountscript.js
+++ WebContent/applicant/accountscript.js
@@ -5,13 +5,12 @@
  */
 
 /**
- * This function loads the account in the system from the database and
- * displays the data.
+ * This function loads the account in the system from the database and displays
+ * the data.
  */
 function loadAccount() {
 	connect("/hiwi/Applicant/js/loadAccount", "", handleLoadAccountResponse);
 }
-
 
 /**
  * This function displays the account's data in the system.
@@ -26,10 +25,10 @@ function handleLoadAccountResponse(mime, data) {
 		window.location = data;
 	} else if (mime == "application/json") {
 		// Evaluating the data
-		var JSONdata = eval("("+data+")");
+		var JSONdata = eval("(" + data + ")");
 		// Filling email and username inputs with old data
 		document.getElementById("newemail").value = JSONdata.email;
-		document.getElementById("newnutzername").value =JSONdata.username;
+		document.getElementById("newnutzername").value = JSONdata.username;
 		// Clearing both password inputs
 		document.getElementById("newpasswort").value = "";
 		document.getElementById("newpasswortwdh").value = "";
@@ -39,8 +38,8 @@ function handleLoadAccountResponse(mime, data) {
 /**
  * Sends request to delete own account.
  */
-function deleteAccount(){
-	connect("/hiwi/Applicant/js/deleteAccount","",handleDeleteResponse);
+function deleteAccount() {
+	connect("/hiwi/Applicant/js/deleteAccount", "", handleDeleteResponse);
 }
 
 /**
@@ -50,23 +49,58 @@ function deleteAccount(){
  * @param data
  */
 function handleDeleteResponse(mime, data) {
-	if(mime=="text/error")alert data;
+	if (mime == "text/error") {
+		// Hier zeigen wir eine Fehlermeldung an wenn solch eine vom Server
+		// kommt:
+		alert(data);
+	} else if (mime == "text/url") {
+		// Hier leiten wir die Seite weiter wenn eine URL vom Server kommt:
+		window.location = data;
+	}
 }
 
-function check(){
+function check() {
+	// Hier die Werte auslesen:
 	var pw = document.getElementById("newpasswort").value;
 	var pww = document.getElementById("newpasswortwdh").value;
 	var mail = document.getElementById("newemail").value;
 	var name = document.getElementById("newnutzername".value);
-	if(document.getElementById("dataconfirm").checked){
-		togglePopup("data_acc_del", true);
-	}else if(pw==pww){
-		changeAccount();
-	}else{
-		
+	// Jetzt müssen wir schauen, ob die leer sind und gültig (hier jetzt nur für
+	// das passwort!). Da mehrere Sachen falsch sein könne, speichere ich den
+	// Wert in einer bool damit immer alle kontrolliert werden, auch wenn einige
+	// fehlen.
+	var error = false;
+	// ACHTUNG: Wenn eines der Passwörter NICHT leer ist, dann sollen sie
+	// geändert
+	// werden, also die werte hashen und mitschicken! Andere Variablen wie Name
+	// und so sollten IMMER nicht leer sein! Siehe auch die register.jsp als
+	// beispiel.
+	if ((pw != null && pw != "") || (pww != null && pww != "")) {
+		// Passwort soll geändert werden, also schauen ob sie gleich sind:
+		if (pw != pww) {
+			// Ja, es gab einen Fehler:
+			error = true;
+			// Denn Bentzer benachrichtigen:
+			toggleWarning("error_unequalPasswords", true,
+					"Passwörter sind nicht gleich!");
+		}
+		// ELSE brauchen wir wenn der Fehler schonmal aufgetreten ist aber jetzt
+		// korrigiert wurde. Dann verstecken wir die (jetzt nicht mehr gültige)
+		// Fehlermeldung wieder.
+		else
+			toggleWarning("error_unequalPasswords", false, "");
 	}
+	// Wenn es einen Fehler gab, den Benutzer die Eingaben kontrollieren lassen:
+	if (error) {
+		return;
+	}
+	// Ansonsten ist alles okay, also weiter:
+	if (document.getElementById("dataconfirm").checked) {
+		togglePopup("data_acc_del", true);
+	}
+	// Hier dann die Änderungen an den Server schicken...
 }
 
-function changeAccount(){
-	
+function changeAccount() {
+
 }
