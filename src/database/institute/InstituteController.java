dac03828d5bf28fdb9475ab3f1cbeba85a23f2ba
Tamino Hartmann <tamino.hartmann@gmail.com>
2012-06-07 18:08:34 +0200

    Angebote werden nun bei löschung von institut durch admin auf institut 0 gesetzt.

--- src/database/institute/InstituteController.java
+++ src/database/institute/InstituteController.java
@@ -10,6 +10,8 @@ import servlet.Helper;
 import database.DatabaseController;
 import database.account.Account;
 import database.account.AccountController;
+import database.offer.Offer;
+import database.offer.OfferController;
 
 public class InstituteController {
 
@@ -18,7 +20,8 @@ public class InstituteController {
 	 */
 	private static InstituteController instController;
 	/**
-	 * AccountController Instanz um beim Loeschen die Institute der Account Tabelle zu updaten
+	 * AccountController Instanz um beim Loeschen die Institute der Account
+	 * Tabelle zu updaten
 	 */
 	private static AccountController acccon;
 
@@ -36,7 +39,7 @@ public class InstituteController {
 
 	private InstituteController() {
 		db = DatabaseController.getInstance();
-		acccon= AccountController.getInstance();
+		acccon = AccountController.getInstance();
 		logger.Log.getInstance().write("InstituteController",
 				"Instance created.");
 	}
@@ -102,17 +105,25 @@ public class InstituteController {
 	}
 
 	/**
-	 * Deletes an institute from the database. All accounts with said Institute will have institute set to 0.
+	 * Deletes an institute from the database. All accounts with said Institute
+	 * will have institute set to 0.
 	 * 
 	 * @param institute
 	 *            The institute to delete.
 	 * @return A flag whether the operation was successful.
 	 */
 	public boolean deleteInstitute(Institute institute) {
+		// Update all accounts:
 		Vector<Account>accs = acccon.getAccountsByInstitute(institute.getIID());
 		for(Account a : accs){
 			a.setInstitute(0);
 			acccon.updateAccount(a);
+		}
+		// Update all offers:
+		Vector<Offer>offers = OfferController.getInstance().getOffersByInstitute(institute.getIID());
+		for(Offer offer: offers){
+			offer.setInstitute(0);
+			OfferController.getInstance().updateOffer(offer);
 		}
 		return db.delete(tableName, "IID=" + institute.getIID());
 	}
@@ -130,57 +141,66 @@ public class InstituteController {
 			return null;
 		}
 	}
+
 	/**
 	 * Gibt alle Institute zurueck die ein Clerk repraesentiert
-	 * @param username Username des Clerks
+	 * 
+	 * @param username
+	 *            Username des Clerks
 	 * @return Alle Institutsnummern in einem Vector
 	 */
-	public Vector<Integer> getAllRepresentingInstitutes(String username){
+	public Vector<Integer> getAllRepresentingInstitutes(String username) {
 		Vector<Integer> ret = new Vector<Integer>();
-		ResultSet rs=db.select(new String[]{"DISTINCT IID"}, new String[]{tableName, "Accounts"}, "Institute.IID=Accounts.institut AND (Accounts.benutzername='"+username+"' OR Accounts.stellvertreter='"+username+"')");
+		ResultSet rs = db.select(new String[] { "DISTINCT IID" }, new String[] {
+				tableName, "Accounts" },
+				"Institute.IID=Accounts.institut AND (Accounts.benutzername='"
+						+ username + "' OR Accounts.stellvertreter='"
+						+ username + "')");
 		try {
-			while(rs.next()){
+			while (rs.next()) {
 				ret.add(rs.getInt(1));
 			}
 		} catch (Exception e) {
-			Helper.log.write("InstituteController", "Error getting representing Institutes");
+			Helper.log.write("InstituteController",
+					"Error getting representing Institutes");
 		}
 		return ret;
 	}
-	
+
 	/**
 	 * Generate a new ID.
-	 * @return
-	 *			generated ID
+	 * 
+	 * @return generated ID
 	 */
-	public int getNewInstID(String tablename){
+	public int getNewInstID(String tablename) {
 		int newID = 0;
 		boolean check = false;
-		while(!check){
+		while (!check) {
 			newID = generateRandomNr(1, 9999);
-			Object[] data = {newID, ""};
-			check =  db.insert(tablename, data );
+			Object[] data = { newID, "" };
+			check = db.insert(tablename, data);
 		}
-		db.delete(tablename, "IID= "+newID);
+		db.delete(tablename, "IID= " + newID);
 		return newID;
 	}
+
 	/**
 	 * Generate a random number.
+	 * 
 	 * @param aStart
-	 * 			Start of the number.
+	 *            Start of the number.
 	 * @param aEnd
-	 * 			End of the number.
-	 * @return
-	 * 		  generated number.
+	 *            End of the number.
+	 * @return generated number.
 	 */
-	private int generateRandomNr(int aStart, int aEnd){
-		
-		    Random random = new Random();
-		    //get the range, casting to long to avoid overflow problems
-		    long range = (long)aEnd - (long)aStart + 1;
-		    // compute a fraction of the range, 0 <= frac < range
-		    long fraction = (long)(range * random.nextDouble());
-		    int randomNumber =  (int)(fraction + aStart);
-		    return randomNumber;
-		  }
+	private int generateRandomNr(int aStart, int aEnd) {
+
+		Random random = new Random();
+		// get the range, casting to long to avoid overflow problems
+		long range = (long) aEnd - (long) aStart + 1;
+		// compute a fraction of the range, 0 <= frac < range
+		long fraction = (long) (range * random.nextDouble());
+		int randomNumber = (int) (fraction + aStart);
+		return randomNumber;
+	}
 }
