4e1aff1f9f583355cf32b8ed77d40422a76ed6d7
Manuel Güntzel <manuel.guentzel@uni-ulm.de>
2012-06-30 15:40:10 +0200

    Garbage collector is drin, benachrichtigung nurnoch wenn gelöschtes angebot nicht abgeschlossen is.

---
+++ src/database/garbage/CollectorTask.java
@@ -0,0 +1,27 @@
+package database.garbage;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.Vector;
+
+import logger.Log;
+
+import database.offer.Offer;
+import database.offer.OfferController;
+
+public class CollectorTask extends TimerTask{
+
+	@Override
+	public void run() {
+		Log.getInstance().write("CollectorTask", "Deleting old offers...");
+		Vector<Offer> toDelete = OfferController.getInstance().getOldOffers(2);
+		if(toDelete==null){
+			System.out.println("Fail");
+			return;
+		}
+		for(Offer o : toDelete){
+			OfferController.getInstance().deleteOffer(o);
+		}
+	}
+	
+}
