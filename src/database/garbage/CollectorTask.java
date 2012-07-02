a9b38658cc2daa6032f205976973b63f18a277e6
Manuel Güntzel <manuel.guentzel@uni-ulm.de>
2012-07-02 22:19:33 +0200

    Javadoc für GarbageCollector

--- src/database/garbage/CollectorTask.java
+++ src/database/garbage/CollectorTask.java
@@ -1,22 +1,20 @@
 /**
- * @author Manuel Guentzel
+ * Dies ist der TimerTask, der das eigentliche loeschen der Angebote uebernimmt.
+ * @author Manuel Güntzel
  */
-package database.garbage;
-
 import java.util.TimerTask;
 import java.util.Vector;
 
 import logger.Log;
 import database.offer.Offer;
 import database.offer.OfferController;
-/**
- * 
- */
+
 public class CollectorTask extends TimerTask{
-	/**
-	 * 
-	 */
+
 	@Override
+	/**
+	 * Run Methode die vom Timer automatisch aufgerufen wird. Sie loescht alle 2 Jahre alten Angebote.
+	 */
 	public void run() {
 		Log.getInstance().write("CollectorTask", "Deleting old offers...");
 		Vector<Offer> toDelete = OfferController.getInstance().getOldOffers(2);
--- src/database/garbage/GarbageCollector.java
+++ src/database/garbage/GarbageCollector.java
@@ -1,46 +1,43 @@
-/**
- * @author Manuel Guentzel
- */
 package database.garbage;
-
+/**
+ * Diese Klasse sorgt dafuer, dass 2 Jahre alte Angebote mit all ihren dazugehoerigen Daten geloescht werden.
+ * @author Manuel Güntzel
+ */
 import java.util.Date;
 import java.util.Timer;
-/**
- * 
- */
-public class GarbageCollector {
 
+public class GarbageCollector {
 	/**
-	 * 
+	 * Timer für das Scheduling des CollectorTasks
 	 */
 	private Timer t;
 	/**
-	 * 
+	 * Instanz dieser Klasse.
 	 */
 	private static GarbageCollector instance;
 	/**
-	 * 
-	 * @return
+	 * Gibt die Instanz des GarbageCollectors zurueck.
+	 * @return Instanz.
 	 */
 	public static GarbageCollector getInstance(){
 		if(instance==null)instance=new GarbageCollector();
 		return instance;
 	}
 	/**
-	 * 
+	 * Private, da Singleton
 	 */
 	private GarbageCollector() {
 		t=new Timer();
 	}
 	/**
-	 * 
+	 * Starten des Timers. Der CollectorTask wird alle 1h gescheduled.
 	 */
 	public void start(){
 		stop();
 		t.scheduleAtFixedRate(new CollectorTask(), new Date(), 3600000);
 	}
 	/**
-	 * 
+	 * Anhalten des Timers. Dabei werden alle Tasks in der Queue geloescht und ein neuer Timer angelegt.
 	 */
 	public void stop(){
 		t.cancel();
