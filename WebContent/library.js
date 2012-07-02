65920b78330b235418e174bf5de49a1886be49e5
Manuel Güntzel <manuel.guentzel@uni-ulm.de>
2012-07-02 13:11:56 +0200

    wirklich letzter email fix

--- WebContent/library.js
+++ WebContent/library.js
@@ -210,7 +210,7 @@ function checkFloat(float) {
 function checkEmail(email) {
 	if (email == "")
 		return false;
-	var regex = /^[a-zA-Z]([a-zA-Z0-9]|[\._\-][a-zA-Z0-9])*\@([a-zA-Z0-9]+|[a-zA-Z0-9]+[\-\.][a-zA-Z0-9]+)\.[a-zA-Z0-9]{2,255}$/;
+	var regex = /^[a-zA-Z0-9]([a-zA-Z0-9]|[\._\-][a-zA-Z0-9])*\@[a-zA-Z0-9]([a-zA-Z0-9]|[\.\-][a-zA-Z0-9])*\.[a-zA-Z0-9]{2,255}$/;
 	return (regex.test(email));
 }
 /**
